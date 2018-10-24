39a6c62296dc3aa0e9a1819e848706d873d415ec
M Rumman Hasan <mrayyanhkhan@outlook.com>
2018-10-24 08:35:29 +0500

    test

--- DiseaseRecom.py
+++ DiseaseRecom.py
@@ -1,6 +1,19 @@
 import io
 
+# data from DB(bridge table);
+# populate that data in List of Array;
+# check most match with symptoms;
+# Match it to doctor profile
+# display doctor
 
-docList = []
+objects = []
 
-class doctorList<>
+Influenza = {"cough", "fever", "headache"}
+Diarrhea = {"motion", "stomach pain"}
+objects.sort()
+objects.insert(1,Influenza)
+objects.insert(2,Diarrhea)
+
+# objects.append(Influenza)
+print(objects)
+# [disese, s1, s2]
