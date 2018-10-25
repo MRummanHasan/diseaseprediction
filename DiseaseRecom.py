eef445f5cfbcb7d70fc7e83e17df951e3c80c138
M Rumman Hasan <mrayyanhkhan@outlook.com>
2018-10-26 02:11:44 +0500

    sql data filtering

--- DiseaseRecom.py
+++ DiseaseRecom.py
@@ -23,10 +23,9 @@ conn = pymysql.connect(host='localhost', user='root', password='', db='HealthSys
 a = conn.cursor()
 # end connection work
 # Query
-
 # sql = 'SELECT D.*, S.* FROM diseases D JOIN disease_symptoms_bridgetable DC ON D.disease_id = DC.disease_id JOIN symptoms S ON S.symptom_id = DC.symptom_id'
-sql = 'SELECT D.disease_name, S.symptom_name FROM diseases D INNER JOIN disease_symptoms_bridgetable DC ON D.disease_id = DC.disease_id INNER JOIN symptoms S ON S.symptom_id = DC.symptom_id'
-      # GROUP BY D.disease_name'
+sql = 'SELECT D.disease_name, S.symptom_name FROM diseases AS D JOIN disease_symptoms_bridgetable AS DC ON D.disease_id = DC.disease_id JOIN symptoms AS S ON S.symptom_id = DC.symptom_id'
+# GROUP BY D.disease_name'
 
 # 'SELECT D.disease_name, S.symptom_name FROM diseases D '
 #     INNER JOIN disease_symptoms_bridgetable DC
@@ -37,25 +36,44 @@ a.execute(sql)
 data = a.fetchall()
 print(data)
 print()
+# AVERAGE CALC FUNCTION
 
-# wordcount={}
-for d in data:
+wordcount = {'Influenza'}
+MAINarr = []
+DS = []
+for word in data:
+    if word[0] not in wordcount:
+        MAINarr.append(DS)
+        print(word[0])
+        DS = [word[0]]
+        wordcount.add(word[0])
+    elif word[0] in wordcount:
+        print(word[1])
+        DS.append(word[1]) # wordcount[word] +=1
+        # print(DS)
+print()
+print(MAINarr)
 
-#     for word in d:
-#         if word not in wordcount:
-#             wordcount[word] = 1
-#         else:
-#             wordcount[word] += 1
-#     print (word,wordcount)
+# for key in wordcount.keys():
+#     print("%s %s " % (key, wordcount[key]))
+# print(wordcount)
 
-    wordcount = {}
-    for word in d:
-        if word not in wordcount:
-            wordcount[word] = 1
-        else:
-            wordcount[word] += 1
-    for key in wordcount.keys():
-        print("%s %s " % (key, wordcount[key]))
+# wordcount = {'Influenza'}
+# for word in data:
+#
+#     if word[0] not in wordcount:
+#         print(word[0])
+#         # wordcount[word] = 1
+#     else:
+#         print(word[1])
+#         # wordcount[word) += 1
+#         DS = [word]
+#         DS.append(word[1])
+#         # wordcount[word]
+#         print("asas")
+#         print(DS)
+# for key in wordcount.keys():
+#     print("%s %s " % (key, wordcount[key]))
 
 
 inputVal = "fever"
@@ -88,16 +106,30 @@ percentArr.sort()
 highestPerValueFound = max(percentArr)
 mostSuitableDisease = percentArr.index(highestPerValueFound)
 
-# doctor ka table 
+# # doctor ka table
+# doctor, disease1,d2,d3
+doctorDisease = [["Doc1","fever","d2","d3"],
+                 ["Doc2","d1","d4","d5"],
+                 ["Doc3","d1","d2","d5"]]
+for doc in doctorDisease:
+    if mostSuitableDisease in doc:
+        print(doc[0] +" "+ " Timings: 2 - 4")
+
+# print(inputVal)
+# print(highestPer)
+# print(per)
+# print(thisDisease)
+#
+# print(percentArr)
+# print(mostSuitableDisease)
 
 
 
-
-
-print(inputVal)
-print(highestPer)
-print(per)
-print(thisDisease)
-
-print(percentArr)
-print(mostSuitableDisease)
+# wordcount={}
+# for d in data:
+#     for word in d:
+#         if word not in wordcount:
+#             wordcount[word] = 1
+#         else:
+#             wordcount[word] += 1
+#     print (word,wordcount)
